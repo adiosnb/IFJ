67bf1418c20e8d1d05f7f9c4f31eb58edf5d02f3
kiko <xbarna02@stud.fit.vutbr.cz>
2016-10-09 22:52:39 +0200

    structures to stab

--- stable.h
+++ stable.h
@@ -3,8 +3,23 @@
 
 #include "instruction_list.h"
 
+typedef struct STAB_ELEMENT{
+    int key;
+    argument_var_t cont;
+    struct STAB_ELEMENT *next;
+}htab_element_t;
+
+typedef struct {
+    unsigned size;
+    unsigned (*hash_fun_ptr)(int, unsigned);
+    unsigned count;
+    htab_element_t *arr[];
+}stable_t;
+
+
+
 argument_var_t *get_var(int id, stable_t *stable);
-int add_var_to_table(int id, argument_var_t *var,stable_t *stable);
+int add_var(int id, argument_var_t *var,stable_t *stable);
 
 
 
