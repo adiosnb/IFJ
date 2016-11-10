c6751639a0cbd139df8debbacd0af01dd8c15218
Adrian Tomasov <atomasov@redhat.com>
2016-11-10 15:52:58 +0100

    interpret: chaged to ptr

--- inter_idea_recursion.c
+++ inter_idea_recursion.c
@@ -9,6 +9,7 @@
 int main() {
 
     argument_var_t tmp_var;
+    argument_var_t *ptr_to_tab;
     instruction_list_t *i_list;
     inter_table_t *inter_tab;
     instruction_item_t *ptr_inst;
@@ -20,13 +21,13 @@ int main() {
     for (int i = 0; i < 20; i++) {
         tmp_var.arg_type = INTEGER;
         tmp_var.data.i = i;
-        inter_table_add_var(inter_tab,i, &tmp_var);
+        inter_table_add_var(inter_tab,(unsigned)i, &tmp_var);
     }
 
     for (int i = 20; i < 40; i++) {
         tmp_var.arg_type = INTEGER;
         tmp_var.data.i = (i - 20) * (-1);
-        inter_table_add_var(inter_tab, i, &tmp_var);
+        inter_table_add_var(inter_tab, (unsigned)i, &tmp_var);
     }
 
 
@@ -42,6 +43,7 @@ int main() {
     tmp_var.arg_type = STACK_EBP;
     tmp_var.data.i = -2;
     inter_table_add_var(inter_tab, ARG_B, &tmp_var);
+    inter_table_add_var(inter_tab, MAIN, &tmp_var);
 
     //definicia lokalnej premennej
     tmp_var.arg_type = STACK_EBP;
@@ -49,52 +51,53 @@ int main() {
     inter_table_add_var(inter_tab, LOCAL_C, &tmp_var);
 
     //program skoci do main
-    create_and_add_instruction(i_list, INST_JMP, MAIN, 0, 0);
+    create_and_add_instruction(i_list, INST_JMP, inter_table_get_var(inter_tab,MAIN), 0, 0);
 
     //funkcia ADD
     ptr_inst = create_and_add_instruction(i_list, INST_LABEL, 0, 0, 0);     //navestie instrukcie
     tmp_var.arg_type = INSTRUCTION;
     tmp_var.data.instruction = ptr_inst;
     inter_table_add_var(inter_tab, ADD, &tmp_var);
-    create_and_add_instruction(i_list, INST_PUSH, 0, 0, 0);//lokalna premenna
-    create_and_add_instruction(i_list, INST_ADD, LOCAL_C, ARG_A, ARG_B);
-    create_and_add_instruction(i_list, INST_WRITE, LOCAL_C, 0, 0);
-    create_and_add_instruction(i_list, INST_JZ, OVER_RECURSION, LOCAL_C, 0);
+    create_and_add_instruction(i_list, INST_PUSH, inter_table_get_var(inter_tab,0), 0, 0);//lokalna premenna
+    create_and_add_instruction(i_list, INST_ADD, inter_table_get_var(inter_tab,LOCAL_C), inter_table_get_var(inter_tab,ARG_A), inter_table_get_var(inter_tab,ARG_B));
+    create_and_add_instruction(i_list, INST_WRITE, inter_table_get_var(inter_tab,LOCAL_C), 0, 0);
+    inter_table_add_var(inter_tab, OVER_RECURSION, &tmp_var);
+    create_and_add_instruction(i_list, INST_JZ, inter_table_get_var(inter_tab,OVER_RECURSION), inter_table_get_var(inter_tab,LOCAL_C), 0);
 
     //rekurzivne volanie funkcie
-    create_and_add_instruction(i_list, INST_PUSH, LOCAL_C, 0, 0);//push cisla 5
-    create_and_add_instruction(i_list, INST_PUSH, 21, 0, 0);//push cisla 1
-    create_and_add_instruction(i_list, INST_CALL, ADD, 0, 0);//volanie funkcie ADD
+    create_and_add_instruction(i_list, INST_PUSH, inter_table_get_var(inter_tab,LOCAL_C), 0, 0);//push cisla 5
+    create_and_add_instruction(i_list, INST_PUSH, inter_table_get_var(inter_tab,21), 0, 0);//push cisla 1
+    create_and_add_instruction(i_list, INST_CALL, inter_table_get_var(inter_tab,ADD), 0, 0);//volanie funkcie ADD
     create_and_add_instruction(i_list, INST_POP, 0, 0, 0);
     create_and_add_instruction(i_list, INST_POP, 0, 0, 0);
 
 
     //preskocenie na koniec programu
     ptr_inst = create_and_add_instruction(i_list, INST_LABEL, 0, 0, 0);
-    tmp_var.arg_type = INSTRUCTION;
-    tmp_var.data.instruction = ptr_inst;
-    inter_table_add_var(inter_tab, OVER_RECURSION, &tmp_var);
+    ptr_to_tab = inter_table_get_var(inter_tab,OVER_RECURSION);
+    ptr_to_tab->arg_type = INSTRUCTION;
+    ptr_to_tab->data.instruction = ptr_inst;
     create_and_add_instruction(i_list, INST_RET, 0, 0, 0);//koniec funkcie ADD
 
 
 
     //MAIN
     ptr_inst = create_and_add_instruction(i_list, INST_WRITE, 10, 0, 0); //echo number 10
-    tmp_var.arg_type = INSTRUCTION;
-    tmp_var.data.instruction = ptr_inst;
-    inter_table_add_var(inter_tab, MAIN, &tmp_var); //ulozenie instrukcie do tabulky
+    ptr_to_tab = inter_table_get_var(inter_tab,MAIN);
+    ptr_to_tab->arg_type = INSTRUCTION;
+    ptr_to_tab->data.instruction = ptr_inst;
 
 
-    create_and_add_instruction(i_list, INST_PUSH, 0, 0, 0);//push cisla 5
-    create_and_add_instruction(i_list, INST_PUSH, 0, 0, 0);//push cisla -1
-    create_and_add_instruction(i_list, INST_CALL, ADD, 0, 0);//volanie funkcie ADD
+    create_and_add_instruction(i_list, INST_PUSH, inter_table_get_var(inter_tab,0), 0, 0);//push cisla 5
+    create_and_add_instruction(i_list, INST_PUSH, inter_table_get_var(inter_tab,0), 0, 0);//push cisla -1
+    create_and_add_instruction(i_list, INST_CALL, inter_table_get_var(inter_tab,ADD), 0, 0);//volanie funkcie ADD
     create_and_add_instruction(i_list, INST_POP, 0, 0, 0);
     create_and_add_instruction(i_list, INST_POP, 0, 0, 0);
 
 
-    create_and_add_instruction(i_list, INST_PUSH, 5, 0, 0);//push cisla 5
-    create_and_add_instruction(i_list, INST_PUSH, 21, 0, 0);//push cisla -1
-    create_and_add_instruction(i_list, INST_CALL, ADD, 0, 0);//volanie funkcie ADD
+    create_and_add_instruction(i_list, INST_PUSH, inter_table_get_var(inter_tab,5), 0, 0);//push cisla 5
+    create_and_add_instruction(i_list, INST_PUSH, inter_table_get_var(inter_tab,21), 0, 0);//push cisla -1
+    create_and_add_instruction(i_list, INST_CALL, inter_table_get_var(inter_tab,ADD), 0, 0);//volanie funkcie ADD
     create_and_add_instruction(i_list, INST_POP, 0, 0, 0);
     create_and_add_instruction(i_list, INST_POP, 0, 0, 0);
 
