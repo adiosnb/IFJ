06e963f79f90749ca5765ad0ea86b8eb153a4462
Adrian Tomasov <atomasov@redhat.com>
2016-11-11 11:01:02 +0100

    STR: added read from stdin

--- str_test.c
+++ str_test.c
@@ -8,12 +8,15 @@ int main() {
     str1 = str_init();
     str2 = str_init();
 
+    printf("\n###################TEST1####################\n");
+
     for (int i=0; i< 300000 ; i++){
         //ADD_CHAR(str1,(i%10)+'0');
         str_add_char(&str1,'0'%10);
     }
 
     str_print(str1);
+    printf("\n###################TEST2####################\n");
 
     str_reinit(&str1);
     for (int i=0; i< 30 ; i++){
@@ -21,7 +24,7 @@ int main() {
     }
     str_print(str1);
     str_reinit(&str1);
-
+    printf("\n###################TEST3####################\n");
     str_append_chars(&str1,"prvy string\n");
     str_append_chars(&str1,"druhy string\n");
 
@@ -30,7 +33,7 @@ int main() {
                    "druhy string\n"
                    "Vystup :\n");
     str_print(str1);
-
+    printf("\n###################TEST4####################\n");
     str_reinit(&str1);
     str_reinit(&str2);
     str_append_chars(&str1,"<toto je string1>");
@@ -40,8 +43,14 @@ int main() {
                    "Ocakavany vystup:\n"
                    "<toto je string1><toto je string2>\n");
     str_print(str1);
+    printf("\n###################TEST5####################\n");
+    str_reinit(&str1);
+    printf("Zadaj data na vstup:");
+    str_read_str_stdin(&str1);
+    str_print(str1);
 
     str_destroy(str1);
+    str_destroy(str2);
 
     return 0;
 }
