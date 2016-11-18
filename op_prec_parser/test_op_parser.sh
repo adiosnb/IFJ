ea8b2deacf6a10aa38e735e0745bcf6c9fb518d3
Adrian Tomasov <atomasov@redhat.com>
2016-11-18 14:45:56 +0100

    Test: new test cases

--- op_prec_parser/test_op_parser.sh
+++ op_prec_parser/test_op_parser.sh
@@ -12,6 +12,8 @@ cp $test_file test_file_backup.txt
 
 rm $test_file
 
+echo "Basic tests for op parser function"
+
 echo "TEST 1"
 echo "i*i" > $test_file
 echo "Expected
@@ -39,8 +41,43 @@ echo "Expected
 i i + i / i + i i i / + * i * i /"
 ./parser
 
+echo "TEST 5"
+echo "(i*i/i+1)/i+(i/i/(i+i*i))/(i+i+i*i*i+i)" > $test_file
+echo "Expected
+i i * i / 1 + i / i i / i i i * + / i i + i i * i * + i + / +"
+./parser
 
-#recovery of test file
+echo "Test for correct finding error"
+
+echo "TEST 6"
+echo "i i - i" > $test_file
+echo "Expected missing operator"
+./parser
+
+echo "TEST 7"
+echo "i + (i - i" > $test_file
+echo "Expected missing parenthesis"
+./parser
+
+echo "TEST 8"
+echo "i + (i - i))" > $test_file
+echo "Expected too many parethesis"
+./parser
+
+echo "TEST 9"
+echo "(i*i/i+1)/i+(i/i/(i+i*i))/(i+i+i i*i+i)" > $test_file
+echo "Expected missing operator"
+./parser
+
+echo
+echo "TEST 10"
+echo "(i*i/i+1)/i+(i/i/((i+i*i))/(i+i+i/i*i+i)" > $test_file
+echo "Expected missing right parenthesis"
+./parser
+
+
+echo
+#cleaning up after tests
 rm $test_file
 mv test_file_backup.txt $test_file
 
