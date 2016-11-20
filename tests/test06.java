c298c4bdbe5bac213cb51543fa0a011787c8e93f
adiosnb <adiosnb@gmail.com>
2016-11-20 22:52:37 +0100

    TESTING: error in test06.java

---
+++ tests/test06.java
@@ -0,0 +1,16 @@
+class Main
+{
+	static void run()
+	{
+		Game.play();
+		Game.play(qopiweyrt);
+		Game.play(qopiweyrt,qwer);
+	}
+}
+class Game
+	{
+	static void play(String str)
+	{
+	    return;
+	}
+}
