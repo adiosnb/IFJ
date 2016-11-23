e3ac048b6678e1dea8fb135753ed7a34f031df65
Adrian Tomasov <atomasov@redhat.com>
2016-11-23 15:16:50 +0100

    TEST: added test 8 and 9 , found errors in parser

---
+++ tests/test08.java
@@ -0,0 +1,12 @@
+class Main {
+    static int trol(){
+        int a = 7;
+        int b = 4;
+        //a = a + a + a * b (b*d);
+        return a;
+    }
+    static void run(){
+        int a = abc;  //error nezname abc
+        return a;
+    }
+}
