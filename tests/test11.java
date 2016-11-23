735a20318a74f15e9acec541e0d2bfeb72f99a41
adiosnb <adiosnb@gmail.com>
2016-11-23 20:49:30 +0100

    TEST: added some tests

---
+++ tests/test11.java
@@ -0,0 +1,12 @@
+class Main {
+    static int fun1(int cislo) {
+        int a;
+        a = b + cislo;
+        int b;
+        return a;
+    }
+
+    static int run(){
+        return 0;
+    }
+}
