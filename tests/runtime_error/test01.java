f246a2fcac5768797fdb31e148045d61f25e5f6c
Axonis <xurban66@vutbr.cz>
2016-11-29 16:27:46 +0100

    TEST: runtime error return types 8 (test02 is correct)

---
+++ tests/runtime_error/test01.java
@@ -0,0 +1,20 @@
+//non-void function without return #8
+
+class Main
+{
+	/*
+		Komentar !!! """
+	*/
+	// Este jedne komentar
+
+    static void run()
+    {
+         test();
+    }
+
+    static int test()
+    {
+        int a = 4;
+        // return a
+    }
+}
---
+++ tests/runtime_error/test02.java
@@ -0,0 +1,14 @@
+//void function return #8
+
+class Main
+{
+    static void run()
+    {
+        double v = voidFunction(2.589);
+    }
+
+    static void voidFunction(double d)
+    {
+        return 12.58;
+    }
+}
